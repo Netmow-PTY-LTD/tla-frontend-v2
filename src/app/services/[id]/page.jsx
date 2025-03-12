ba1578971bdee95b4722e841a9b5a30505cb8191
rabby.dev <rabby.netmow@gmail.com>
2025-03-12 14:44:24 +0600

    complte services route page ui

--- src/app/services/[id]/page.jsx
+++ src/app/services/[id]/page.jsx
@@ -1,55 +1,41 @@
+import FamilyLawCard from "@/components/main/common/card/FamilyLawCard";
+import ServiceDetailsCard from "@/components/main/common/card/ServiceDetailsCard";
 import MainLayout from "@/components/main/common/layout";
+
 import Image from "next/image";
+import Link from "next/link";
 import React from "react";
 
-const SingleServiceDetailsPage = () => {
+const SingleServiceDetailsPage = async ({ params }) => {
+  const { id } = await params;
+
   return (
     <MainLayout>
-      <div className="flex flex-col lg:flex-row items-center lg:items-start mx-4 lg:mx-[212px] my-[58px]">
-        <div className="text-center lg:text-left lg:w-1/2 px-4 my-10 lg:mr-[155px]">
-          <button className="bg-[#F3F3F3] py-1 px-5 rounded-[82px] font-medium flex items-center mx-auto lg:mx-0">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="24"
-              height="24"
-              viewBox="0 0 24 24"
-            >
-              <path
-                fill="currentColor"
-                d="M11.03 8.53a.75.75 0 1 0-1.06-1.06l-4 4a.75.75 0 0 0 0 1.06l4 4a.75.75 0 1 0 1.06-1.06l-2.72-2.72H18a.75.75 0 0 0 0-1.5H8.31z"
-              />
-            </svg>{" "}
-            <span className="ml-1"> Family Law</span>
-          </button>
-          <h1 className="font-bold text-2xl lg:text-4xl my-5">
-            Family Law Services – Expert Legal Help for Your Family Matters
-          </h1>
-          <p className="text-[#34495E] mb-10">
-            Family law covers legal matters related to family relationships,
-            including marriage, divorce, child custody, and financial support.
-            Whether you're seeking legal protection, mediation, or court
-            representation, having an experienced family lawyer by your side
-            ensures your rights are protected and the best outcome is achieved.
-          </p>
-          <div className="flex justify-center lg:justify-start">
-            <button className="btn-brand ">Get Family Lawyers Today</button>
+      <section className="mx-4 lg:mx-[212px] my-[58px]">
+        <div>
+          <ServiceDetailsCard />
+        </div>
+
+        <div className="mt-[132px] ">
+          <div className="text-center">
+            <h1 className="font-bold  text-2xl lg:text-4xl mt-5">
+              Common Family Law Issues & Solutions
+            </h1>
+            <p className="text-[#34495E] mb-10">
+              Finding legal help has never been simpler. Whether you're looking
+              for a lawyer or offering legal services, The Law App streamlines
+              the process in just three steps:
+            </p>
+          </div>
+
+          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6 lg:gap-[30px] ">
+            <FamilyLawCard />
+            <FamilyLawCard />
+            <FamilyLawCard />
+            <FamilyLawCard />
           </div>
         </div>
-        <figure className="mt-6 lg:mt-0 lg:w-1/2 w-full">
-          <Image
-            alt="law-picture"
-            src="/assets/img/services/img1.png"
-            width={1200}
-            height={800}
-            sizes="100vw"
-            className="rounded-xl"
-            style={{
-              width: "100%",
-              height: "auto",
-            }}
-          />
-        </figure>
-      </div>
+      </section>
     </MainLayout>
   );
 };
--- src/components/main/common/card/LawCard.jsx
+++ src/components/main/common/card/LawCard.jsx
@@ -5,7 +5,7 @@ import React from "react";
 const LawCard = ({ service }) => {
   return (
     <Link href={`/services/${service._id}`}>
-      <div className="law-card">
+      <div className="law-card max-w-sm sm:max-w-md md:max-w-lg lg:max-w-xl xl:max-w-2xl mx-auto">
         <figure style={{ position: "relative", height: "400px" }}>
           <Image
             alt="law-picture"
@@ -18,8 +18,8 @@ const LawCard = ({ service }) => {
             }}
           />
         </figure>
-        <div className="card-body bg-[#EDF0F4] mt-2 rounded-xl p-3 ">
-          <div className="flex justify-between items-center">
+        <div className="card-body bg-[#EDF0F4] mt-2 rounded-xl p-3 sm:p-4 ">
+          <div className="flex justify-between items-center gap-2">
             <p>
               {" "}
               <span className="text-[#34495E] ">Case Listed:</span>{" "}
@@ -31,7 +31,7 @@ const LawCard = ({ service }) => {
               <span className="font-semibold">{service?.lawyerAvailable} </span>
             </p>
           </div>
-          <h1 className="card-title font-medium text-[27px] mt-2 ">
+          <h1 className="card-title font-medium sm:text-2xl text-[27px] mt-2 ">
             {service?.lawName}{" "}
           </h1>
         </div>
