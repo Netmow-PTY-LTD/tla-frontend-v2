fba22e2edf00d5cc015d3970722f93ec88113031
rabby.dev <rabby.netmow@gmail.com>
2025-03-12 12:10:53 +0600

    create single service details page

---
+++ src/app/services/[id]/page.jsx
@@ -0,0 +1,12 @@
+import MainLayout from "@/components/main/common/layout";
+import React from "react";
+
+const SingleServiceDetailsPage = () => {
+  return (
+    <MainLayout>
+      <h2>Single Service Details page</h2>
+    </MainLayout>
+  );
+};
+
+export default SingleServiceDetailsPage;
--- src/components/main/common/card/LawCard.jsx
+++ src/components/main/common/card/LawCard.jsx
@@ -1,40 +1,42 @@
 import Image from "next/image";
+import Link from "next/link";
 import React from "react";
 
 const LawCard = ({ service }) => {
-  console.log(service);
   return (
-    <div className="law-card">
-      <figure style={{ position: "relative", height: "400px" }}>
-        <Image
-          alt="law-picture"
-          src={service?.image}
-          fill
-          sizes="(min-width: 808px) 50vw, 100vw"
-          className="rounded-xl"
-          style={{
-            objectFit: "cover", // cover, contain, none
-          }}
-        />
-      </figure>
-      <div className="card-body bg-[#EDF0F4] mt-2 rounded-xl p-3 ">
-        <div className="flex justify-between items-center">
-          <p>
-            {" "}
-            <span className="text-[#34495E] ">Case Listed:</span>{" "}
-            <span className="font-semibold">{service?.caseListed} </span>
-          </p>
-          <p>
-            {" "}
-            <span className="text-[#34495E] ">Lawyer Available:</span>{" "}
-            <span className="font-semibold">{service?.lawyerAvailable} </span>
-          </p>
+    <Link href={`/services/${service._id}`}>
+      <div className="law-card">
+        <figure style={{ position: "relative", height: "400px" }}>
+          <Image
+            alt="law-picture"
+            src={service?.image}
+            fill
+            sizes="(min-width: 808px) 50vw, 100vw"
+            className="rounded-xl"
+            style={{
+              objectFit: "cover", // cover, contain, none
+            }}
+          />
+        </figure>
+        <div className="card-body bg-[#EDF0F4] mt-2 rounded-xl p-3 ">
+          <div className="flex justify-between items-center">
+            <p>
+              {" "}
+              <span className="text-[#34495E] ">Case Listed:</span>{" "}
+              <span className="font-semibold">{service?.caseListed} </span>
+            </p>
+            <p>
+              {" "}
+              <span className="text-[#34495E] ">Lawyer Available:</span>{" "}
+              <span className="font-semibold">{service?.lawyerAvailable} </span>
+            </p>
+          </div>
+          <h1 className="card-title font-medium text-[27px] mt-2 ">
+            {service?.lawName}{" "}
+          </h1>
         </div>
-        <h1 className="card-title font-medium text-[27px] mt-2 ">
-          {service?.lawName}{" "}
-        </h1>
       </div>
-    </div>
+    </Link>
   );
 };
 
