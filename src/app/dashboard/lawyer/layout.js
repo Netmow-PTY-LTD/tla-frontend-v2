9385c2e2685bce170b11193eb911b56d71243b9b
rabby.dev <rabby.netmow@gmail.com>
2025-03-18 09:03:53 +0600

    modified lawyer layout

--- src/app/dashboard/lawyer/layout.js
+++ src/app/dashboard/lawyer/layout.js
@@ -1,8 +1,8 @@
-import DashboardFooter from "@/components/dashboard/common/DashboardFooter";
-import DashboardHeader from "@/components/dashboard/common/DashboardHeader";
-import SideNav from "@/components/dashboard/common/SideNav";
-import React from "react";
-import "@/styles/dashboard.css";
+import DashboardFooter from '@/components/dashboard/common/DashboardFooter';
+import DashboardHeader from '@/components/dashboard/common/DashboardHeader';
+import SideNav from '@/components/dashboard/common/SideNav';
+import React from 'react';
+import '@/styles/dashboard.css';
 
 export default function DashboardLayout({ children }) {
   return (
