65e5aa5064dcaf04158e4d8ca408540b177ab2d7
rabby.dev <rabby.netmow@gmail.com>
2025-03-18 12:14:31 +0600

    added response card

--- src/app/dashboard/lawyer/my-response/page.js
+++ src/app/dashboard/lawyer/my-response/page.js
@@ -1,3 +1,4 @@
+import LeadCard from '@/components/dashboard/lawyer-dashboard/components/LeadCard';
 import StatusButton from '@/components/dashboard/lawyer-dashboard/components/StatusButton';
 import React from 'react';
 
@@ -11,6 +12,14 @@ const MyResponsePage = () => {
           <StatusButton status="pending" />
           <StatusButton status="hired" />
         </div>
+      </section>
+      <section className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 gap-5 mt-4">
+        <LeadCard />
+        <LeadCard />
+        <LeadCard />
+        <LeadCard />
+        <LeadCard />
+        <LeadCard />
       </section>
     </div>
   );
