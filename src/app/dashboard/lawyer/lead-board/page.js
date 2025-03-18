bac4d6c921abd38d0a1957904115fadb0e0ad81a
rabby.dev <rabby.netmow@gmail.com>
2025-03-18 10:47:40 +0600

    create leads board heder section ui

--- src/app/dashboard/lawyer/lead-board/page.js
+++ src/app/dashboard/lawyer/lead-board/page.js
@@ -1,21 +1,54 @@
+import TagButton from '@/components/dashboard/lawyer-dashboard/components/TagButton';
+import {
+  Database,
+  DatabaseBackupIcon,
+  FilterIcon,
+  SlidersVertical,
+} from 'lucide-react';
 import Link from 'next/link';
 import React from 'react';
 
 const LeadBoardPage = () => {
   return (
     <div>
-      <section>
-        <h2>Founded 40 Matching Leads</h2>
-        <div>
-          <Link href={'#'}>
-            {' '}
-            <span>icon</span> Showing all 40 leads -{' '}
-          </Link>{' '}
-          |{' '}
+      <section className="px-4">
+        <h1 className="font-bold text-lg md:text-2xl text-[#0B1C2D] text-center md:text-left">
+          Founded 40 Matching Leads
+        </h1>
+
+        <div className="flex flex-wrap md:flex-nowrap justify-center md:justify-between items-center my-3 gap-4">
+          <div className="flex flex-wrap items-center gap-2 text-[#34495E] justify-center">
+            <Link href="#" className="flex items-center gap-2">
+              <Database className="w-5 h-5" />
+              <span>Showing all 40 leads -</span>
+            </Link>
+
+            <span className="hidden md:inline">|</span>
+
+            <div className="flex items-center gap-2">
+              <h2>Services:</h2>
+              <div className="inline-flex flex-wrap gap-2">
+                <TagButton text="Family Law" bgColor="#FF86021A" />
+                <TagButton text="Divorce Law" bgColor="#004DA61A" />
+                <TagButton text="1+ more" bgColor="#7070701A" />
+              </div>
+            </div>
+
+            <span className="hidden md:inline">|</span>
+
+            <h2 className="text-center md:text-left">
+              <span>Location:</span> Elgin St. Celina, Delaware...
+            </h2>
+          </div>
+
           <div>
-            <h2>Services: </h2>
+            <button className="font-medium text-[#0194EF] flex items-center gap-2">
+              <span>Filter Result</span> <SlidersVertical />
+            </button>
           </div>
         </div>
+
+        <hr className="border border-[#F3F3F3]" />
       </section>
     </div>
   );
