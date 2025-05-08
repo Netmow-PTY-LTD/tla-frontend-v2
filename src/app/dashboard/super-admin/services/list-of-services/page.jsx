3609a11cb1004ebe9d506fa0ee1d99092418d787
rabby <rabby.netmow@gmail.com>
2025-05-08 10:54:11 +0600

    fixed redux state and service manage naming convention

--- src/app/dashboard/super-admin/services/list-of-services/page.jsx
+++ src/app/dashboard/super-admin/services/list-of-services/page.jsx
@@ -25,7 +25,7 @@ import {
   useAllServicesQuery,
   useChangeServicesStatusMutation,
   useSingleServicesQuery,
-} from '@/store/slices/super-admin/servicesSlice';
+} from '@/store/API/super-admin/servicesApiService';
 import { CheckCircle, CircleX, MoreHorizontal, Pencil } from 'lucide-react';
 import React, { useState } from 'react';
 import LoaderPage from '@/components/common/LoaderPage';
