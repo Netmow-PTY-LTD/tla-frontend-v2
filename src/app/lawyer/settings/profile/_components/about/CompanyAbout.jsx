24c3d0192a9541fcc0baa3947f115f1b0ee46f82
rabby <rabby.netmow@gmail.com>
2025-05-20 13:14:40 +0600

    added dropdwon select

--- src/app/lawyer/settings/profile/_components/about/CompanyAbout.jsx
+++ src/app/lawyer/settings/profile/_components/about/CompanyAbout.jsx
@@ -3,11 +3,37 @@
 import TextArea from '@/components/form/TextArea';
 import TextInput from '@/components/form/TextInput';
 import { Form } from '@/components/ui/form';
+import DropdownMenuSelect from '@/components/UIComponents/DropDwonMenuSelect';
 import React from 'react';
 import { useForm } from 'react-hook-form';
 
 export default function CompanyAbout() {
   const form = useForm();
+  const options = [
+    {
+      label: 'Self-employed / Sole Trader',
+      value: 'self_employed',
+    },
+    {
+      label: '2–10 Employees',
+      value: '2_10_employees',
+    },
+    {
+      label: '11–50 Employees',
+      value: '11_50_employees',
+    },
+    {
+      label: '51–200 Employees',
+      value: '51_200_employees',
+    },
+    {
+      label: 'Over 200 Employees',
+      value: 'over_200_employees',
+    },
+  ];
+  const handleSelect = (value) => {
+    console.log('Selected:', value);
+  };
   return (
     <div className="py-9">
       <h2 className="text-black font-semibold">About the company</h2>
@@ -15,11 +41,12 @@ export default function CompanyAbout() {
 
       <Form {...form}>
         <div className="grid grid-cols-2 gap-x-20 gap-y-7">
-          <TextInput
-            control={form.control}
-            label="Years in business"
-            name="yer"
-            placeholder="Number of years"
+          <DropdownMenuSelect
+            triggerLabel="Company Size"
+            dropdownLabel="Choose Company Size"
+            options={options}
+            onSelect={handleSelect}
+            defaultValue="Self-employed / Sole Trader"
           />
 
           <TextInput
