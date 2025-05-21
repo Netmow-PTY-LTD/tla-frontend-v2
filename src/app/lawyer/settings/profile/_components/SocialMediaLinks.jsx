7c527f97ba77e6a89417f81f581514bce01986a2
rabby <rabby.netmow@gmail.com>
2025-05-21 09:23:15 +0600

    making social media accordion page ui design

--- src/app/lawyer/settings/profile/_components/SocialMediaLinks.jsx
+++ src/app/lawyer/settings/profile/_components/SocialMediaLinks.jsx
@@ -1,14 +1,31 @@
+'use client';
 import React from 'react';
 import SocialMediaLink from './social-media/SocialMediaLink';
 import ExternalLinks from './social-media/ExternalLinks';
 
 export default function SocialMediaLinks() {
+  const onSave = () => console.log('Save clicked');
+  const onCancel = () => console.log('Cancel clicked');
   return (
     <div>
-      <h2 className="font-bold text-lg">Social media & links</h2>
       <div className="flex flex-col gap-3">
         <SocialMediaLink />
-        <ExternalLinks />
+        {/* <ExternalLinks /> */}
+        {/* Footer Buttons */}
+        <div className="flex justify-between items-center pt-4 ">
+          <button
+            onClick={onCancel}
+            className="text-sm text-gray-600 hover:text-gray-800"
+          >
+            Cancel
+          </button>
+          <button
+            onClick={onSave}
+            className="bg-[#12C7C4] text-white px-4 py-2 text-sm rounded-md hover:bg-[#10b0ae]"
+          >
+            Save
+          </button>
+        </div>
       </div>
     </div>
   );
