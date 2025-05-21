1ca013d756c5f7d7a543ecf8050439fb823b519d
rabby <rabby.netmow@gmail.com>
2025-05-21 11:07:25 +0600

    added accreditaion accordion page ui design

--- src/app/lawyer/settings/profile/_components/Accreditations.jsx
+++ src/app/lawyer/settings/profile/_components/Accreditations.jsx
@@ -1,14 +1,56 @@
+'use client';
+
 import React from 'react';
 import AddAccreditationsModal from './accreditations/AddAccreditationsModal';
 import AccreditationsList from './accreditations/AccreditationsList';
+import { CircleAlert } from 'lucide-react';
+import ToggleSwitch from '@/components/UIComponents/ToggleSwitch';
 
 export default function Accreditations() {
+  const handleToggle = (checked) => {
+    console.log('Switch is now:', checked);
+  };
+  const onSave = () => console.log('Save clicked');
+  const onCancel = () => console.log('Cancel clicked');
+
   return (
-    <div>
-      <h2 className="font-bold text-lg">Accreditations</h2>
-      <div className="flex flex-col gap-3">
-        <AccreditationsList />
-        <AddAccreditationsModal />
+    <div className="max-w-[900px]">
+      <div>
+        <div className="flex items-center justify-between gap-5">
+          <div>
+            <h2 className="text-base font-semibold text-black">
+              Accreditations
+            </h2>
+            <p className="text-sm text-[#8E8E8E] mt-2">
+              Increase your chances of getting hired and boost customer
+              confidence by adding your accreditations.
+            </p>
+          </div>
+          <div className="flex items-center gap-2 max-w-sm p-2 rounded-md text-gray-700 text-sm">
+            <CircleAlert className="w-4 h-4" />
+            <span>Optional</span>
+            <ToggleSwitch onToggle={handleToggle} />
+          </div>
+        </div>
+        <div className="flex flex-col gap-3">
+          <AccreditationsList />
+          <AddAccreditationsModal />
+        </div>
+      </div>
+      {/* Footer Buttons */}
+      <div className="flex justify-between items-center pt-4 ">
+        <button
+          onClick={onCancel}
+          className="text-sm text-gray-600 hover:text-gray-800"
+        >
+          Cancel
+        </button>
+        <button
+          onClick={onSave}
+          className="bg-[#12C7C4] text-white px-4 py-2 text-sm rounded-md hover:bg-[#10b0ae]"
+        >
+          Save
+        </button>
       </div>
     </div>
   );
--- src/app/lawyer/settings/profile/_components/accreditations/AccreditationsList.jsx
+++ src/app/lawyer/settings/profile/_components/accreditations/AccreditationsList.jsx
@@ -1,5 +1,34 @@
+import { Edit, FileIcon, Trash } from 'lucide-react';
 import React from 'react';
 
 export default function AccreditionsList() {
-  return <div>AccreditionsList</div>;
+  return (
+    <div className="mt-5">
+      <div className="bg-[#f3f3f3] border  rounded-sm shadow p-4 w-full  flex items-center justify-between">
+        <div className="flex items-center gap-2 ">
+          <h1 className=" font-semibold">Title</h1>
+          <span>
+            <FileIcon className="w-4 h-4" />
+          </span>
+        </div>
+
+        <div className="flex justify-end gap-3 ">
+          <button
+            type="button"
+            className="p-2 rounded-md hover:bg-gray-100 transition"
+            title="Edit"
+          >
+            <Edit className="w-4 h-4 text-gray-600" />
+          </button>
+          <button
+            type="button"
+            className="p-2 rounded-md hover:bg-gray-100 transition"
+            title="Trust"
+          >
+            <Trash className="w-4 h-4 text-gray-600" />
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 }
