4c9a80683cedcef038d8fe2b07605952fca78753
rabby <rabby.netmow@gmail.com>
2025-05-23 16:26:21 +0600

    fixed margin

--- src/app/lawyer/settings/lead-settings/page.jsx
+++ src/app/lawyer/settings/lead-settings/page.jsx
@@ -1,19 +1,22 @@
 'use client';
 import React from 'react';
-import { DynamicAccordion } from '@/components/UIComponents/AcordionComponent';
+// import { DynamicAccordion } from '@/components/UIComponents/AcordionComponent';
 import ServicesList from './_components/MyServices';
 
 const LeadSettingsPage = () => {
-  const accordionItems = [
-    {
-      id: 'my-services',
-      title: 'My Services',
-      content: <ServicesList />,
-    },
-  ];
+  // const accordionItems = [
+  //   {
+  //     id: 'my-services',
+  //     title: 'My Services',
+  //     content: <ServicesList />,
+  //   },
+  // ];
   return (
-    <div>
-      <DynamicAccordion items={accordionItems} />
+    <div className="w-full">
+      {/* <DynamicAccordion items={accordionItems} /> */}
+      <div className="max-w-[900px] mx-auto">
+        <ServicesList />
+      </div>
     </div>
   );
 };
