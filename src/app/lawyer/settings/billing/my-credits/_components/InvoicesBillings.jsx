ffdb9d90831afe93003e6ec6ce4dec04255baae5
rabby <rabby.netmow@gmail.com>
2025-05-23 11:59:04 +0600

    added lawyer dashboard settings layout my creadite tab ui design

---
+++ src/app/lawyer/settings/billing/my-credits/_components/InvoicesBillings.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const InvoicesBillings = () => {
+  return (
+    <div>
+      <h2>Invoices And billing</h2>
+    </div>
+  );
+};
+
+export default InvoicesBillings;
---
+++ src/app/lawyer/settings/billing/my-credits/_components/MyCreadits.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const MyCredits = () => {
+  return (
+    <div>
+      <h2>My Credits</h2>
+    </div>
+  );
+};
+
+export default MyCredits;
