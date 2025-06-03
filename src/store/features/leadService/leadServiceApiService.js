4a57d8fc85bd0f0d3776ed00dbb5236c6831ffe1
rabby <rabby.netmow@gmail.com>
2025-06-03 15:04:23 +0600

    added lead service related api

---
+++ src/store/features/leadService/leadServiceApiService.js
@@ -0,0 +1,31 @@
+import { baseApi } from '../../baseApi/baseApi';
+
+const leadServiceApiService = baseApi.injectEndpoints({
+  endpoints: (builder) => ({
+    addLeadService: builder.mutation({
+      query: (body) => ({
+        url: '/lead-service/add',
+        method: 'POST',
+        body,
+      }),
+      invalidatesTags: ['leadService'],
+    }),
+    getLeadServiceList: builder.query({
+      query: () => ({
+        url: `lead-service/list`,
+        method: 'GET',
+      }),
+      invalidatesTags: ['leadService'],
+    }),
+
+    deleteRange: builder.mutation({
+      query: (id) => ({
+        url: `/lead-service/${id}`,
+        method: 'DELETE',
+      }),
+      invalidatesTags: ['leadService'],
+    }),
+  }),
+});
+
+export const {} = leadServiceApiService;
