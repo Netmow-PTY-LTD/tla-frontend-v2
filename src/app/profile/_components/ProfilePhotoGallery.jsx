6e763b899daa5fca746a826ee943b47a97e01b01
Tuhin Alom <tuhin.netmow@gmail.com>
2025-06-26 14:59:49 +0600

    fixed a lot of issues

--- src/app/profile/_components/ProfilePhotoGallery.jsx
+++ src/app/profile/_components/ProfilePhotoGallery.jsx
@@ -23,32 +23,8 @@ export default function ProfilePhotoGallery({ userInfo }) {
     <section className="py-5 relative">
       <div className="flex flex-wrap gap-5">
         <div className="w-full">
-          <h2 className="text-[24px] font-semibold mb-4 profile-heading relative flex items-baseline gap-3">
+          <h2 className="text-[#00C3C0] font-bold mb-4 profile-heading relative flex items-baseline gap-3">
             <span>Galleries</span>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="62"
-              height="4"
-              viewBox="0 0 62 4"
-              fill="none"
-            >
-              <rect
-                x="0.138672"
-                y="0.201172"
-                width="11.3115"
-                height="3.40625"
-                rx="1.70312"
-                fill="#D9D9D9"
-              />
-              <rect
-                x="17.4512"
-                y="0.201172"
-                width="44.5493"
-                height="3.40625"
-                rx="1.70312"
-                fill="#00C3C0"
-              />
-            </svg>
           </h2>
           <div className="grid grid-cols-2 md:grid-cols-4 gap-4 mt-4">
             {photos.length > 0 ? (
