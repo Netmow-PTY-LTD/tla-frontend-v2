0e31db037c3e4f05ad7a98891df560985523b677
rabby <rabby.netmow@gmail.com>
2025-07-08 22:34:55 -0700

    create credit related acrodion and conntent ui design  in credit purchase ui modal

--- src/components/UIComponents/AcordionComponent.jsx
+++ src/components/UIComponents/AcordionComponent.jsx
@@ -16,12 +16,12 @@ const AccordionComponent = ({ title, content }) => {
       <Accordion
         type="single"
         collapsible
-        className="bg-[#F3F3F3] p-[10px] rounded-[5px]  "
+        className="bg-[#F3F3F3]  rounded-[5px]  "
       >
-        <AccordionItem value="item-1" className="border-none">
+        <AccordionItem value={title} className="border-none">
           <AccordionTrigger className="h-[44px] px-2">
             <div className="flex items-center gap-[10px] ">
-              <CircularProgress progress={27} size={30} />
+              {/* <CircularProgress progress={27} size={30} /> */}
               <span className="text-sm font-medium text-gray-700 leading-none">
                 {title}
               </span>
