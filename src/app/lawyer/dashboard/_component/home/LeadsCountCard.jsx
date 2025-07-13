215399e5a0f51b64789e85c9cb798d05877cd111
rabby <rabby.netmow@gmail.com>
2025-07-13 10:07:20 +0600

    added response stats in dashboard

--- src/app/lawyer/dashboard/_component/home/LeadsCountCard.jsx
+++ src/app/lawyer/dashboard/_component/home/LeadsCountCard.jsx
@@ -65,20 +65,20 @@ import { Button } from '@/components/ui/button';
 import Link from 'next/link';
 import {
   useGetAllLeadsQuery,
-  useGetAllMyLeadsQuery,
 } from '@/store/features/lawyer/LeadsApiService';
 
 export default function LeadsCountCard() {
-  const { data: allLeads, isLoading } = useGetAllMyLeadsQuery({
+  const { data: allLeads, isLoading } = useGetAllLeadsQuery({
     page: 1,
     limit: 10,
   });
 
   const totalLeads = allLeads?.pagination?.total ?? 0;
   const approvedLeads =
-    allLeads?.data?.filter((lead) => lead.status === 'approved')?.length ?? 0;
+    allLeads?.data?.filter((lead) => lead.status === 'approved')?.length;
   const pendingLeads =
-    allLeads?.data?.filter((lead) => lead.status === 'pending')?.length ?? 0;
+    allLeads?.data?.filter((lead) => lead.status === 'pending')?.length;
+
 
   return (
     <Card className="w-full shadow-sm rounded-2xl">
@@ -99,34 +99,42 @@ export default function LeadsCountCard() {
       <div className="flex items-center justify-center h-[calc(100%-100px)]">
         <div className="flex items-center justify-center gap-6 p-4">
           {/* Total */}
-          <div className="flex flex-col items-center gap-1">
-            <div className="flex items-center justify-center w-16 h-16 rounded-full bg-slate-900 text-white">
-              <p className="text-2xl font-bold">
-                {isLoading ? '...' : totalLeads}
-              </p>
+          {
+            totalLeads > 0 && <div className="flex flex-col items-center gap-1">
+              <div className="flex items-center justify-center w-16 h-16 rounded-full bg-slate-900 text-white">
+                <p className="text-2xl font-bold">
+                  {isLoading ? '...' : totalLeads}
+                </p>
+              </div>
+              <span className="text-sm font-medium mt-1">Total</span>
             </div>
-            <span className="text-sm font-medium mt-1">Total</span>
-          </div>
+          }
 
           {/* Approved */}
-          <div className="flex flex-col items-center gap-1">
-            <div className="flex items-center justify-center w-16 h-16 rounded-full bg-green-600 text-white">
-              <p className="text-2xl font-bold">
-                {isLoading ? '...' : approvedLeads}
-              </p>
+          {
+            approvedLeads > 0 && <div className="flex flex-col items-center gap-1">
+              <div className="flex items-center justify-center w-16 h-16 rounded-full bg-green-600 text-white">
+                <p className="text-2xl font-bold">
+                  {isLoading ? '...' : approvedLeads}
+                </p>
+              </div>
+              <span className="text-sm font-medium mt-1">Approved</span>
             </div>
-            <span className="text-sm font-medium mt-1">Approved</span>
-          </div>
+          }
+
 
           {/* Pending */}
-          <div className="flex flex-col items-center gap-1">
-            <div className="flex items-center justify-center w-16 h-16 rounded-full bg-yellow-500 text-white">
-              <p className="text-2xl font-bold">
-                {isLoading ? '...' : pendingLeads}
-              </p>
+
+          {
+            pendingLeads > 0 && <div className="flex flex-col items-center gap-1">
+              <div className="flex items-center justify-center w-16 h-16 rounded-full bg-yellow-500 text-white">
+                <p className="text-2xl font-bold">
+                  {isLoading ? '...' : pendingLeads}
+                </p>
+              </div>
+              <span className="text-sm font-medium mt-1">Pending</span>
             </div>
-            <span className="text-sm font-medium mt-1">Pending</span>
-          </div>
+          }
         </div>
       </div>
     </Card>
