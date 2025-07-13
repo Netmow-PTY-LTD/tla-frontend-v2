d062f0d9ab0017741f20ebcbed80e38400ee0c2d
rabby <rabby.netmow@gmail.com>
2025-07-13 14:31:12 +0600

    added  notification related api

--- src/store/features/notification/notificationApiService.js
+++ src/store/features/notification/notificationApiService.js
@@ -16,14 +16,29 @@ const notificationApiService = baseApi.injectEndpoints({
         method: 'PUT',
         body,
       }),
-      invalidatesTags: ['notification'],
+      invalidatesTags: ['notificationPreferences'],
     }),
     getAllNotifications: builder.query({
       query: () => ({
         url: `/settings/notification`,
         method: 'GET',
       }),
+      providesTags: ['notificationPreferences'],
+    }),
+    getAllNotificationsPreferences: builder.query({
+      query: () => ({
+        url: `/settings/notification/preferences`,
+        method: 'GET',
+      }),
       providesTags: ['notification'],
+    }),
+     markAsRedNotification: builder.mutation({
+      query: (body) => ({
+        url: `/settings/notification/${body.notificationId}/red`,
+        method: 'PUT',
+        body:body.data,
+      }),
+      invalidatesTags: ['notification'],
     }),
   }),
 });
@@ -32,4 +47,7 @@ export const {
   useUpdateEmailNotificationMutation,
   useUpdateBrowserNotificationMutation,
   useGetAllNotificationsQuery,
+  useGetAllNotificationsPreferencesQuery,
+  useMarkAsRedNotificationMutation
+
 } = notificationApiService;
