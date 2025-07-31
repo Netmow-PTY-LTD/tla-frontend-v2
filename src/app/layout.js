25db56307cda64a542b00f8ec682e66478189815
rabby <rabby.netmow@gmail.com>
2025-07-31 16:19:18 +0600

    fixed

--- src/app/layout.js
+++ src/app/layout.js
@@ -20,7 +20,7 @@ const poppins = Poppins({
 export const metadata = {
   title: 'The LawAppOnline',
   description:
-    'The Law App is a complete online marketplace for people to search for lawyers at a price they can afford and for lawyers to build an online presence to find clients without the need for heavy marketing expenses. We match clients to lawyers directly based on their field of expertise and allow fair bidding to reach the right price.',
+    'The Law App is a  complete online marketplace for people to search for lawyers at a price they can afford and for lawyers to build an online presence to find clients without the need for heavy marketing expenses. We match clients to lawyers directly based on their field of expertise and allow fair bidding to reach the right price.',
 };
 
 export default function RootLayout({ children }) {
