98ae6843e5411f7176c845d6d2c74594d0f2aeb3
Tuhin Alom <tuhin.netmow@gmail.com>
2025-08-03 20:40:18 +0600

    scroll issue in my leads page in client dashboard

--- src/components/main/home/HomeCategoryWiseServices.jsx
+++ src/components/main/home/HomeCategoryWiseServices.jsx
@@ -92,7 +92,7 @@ export default function HomeCategoryWiseServices() {
           <p>The best services for you</p>
         </div> */}
         {allServices?.length > 0 && (
-          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-10 mt-16">
+          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-8 mt-16">
             {allServices?.map((service, i) => (
               <Link
                 key={service.id || i}
