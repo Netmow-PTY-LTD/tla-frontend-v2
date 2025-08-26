5c33252537e093789c25d530efd37b9be4dd0eda
rabby <rabby.netmow@gmail.com>
2025-08-26 09:55:08 +0600

    added fearure of aviable credit shwo lawywer dashboard header and fixed rating feed back content

--- src/components/dashboard/common/DashboardHeader.jsx
+++ src/components/dashboard/common/DashboardHeader.jsx
@@ -5,13 +5,15 @@ import React from 'react';
 import ProfileDropDown from './ProfileDropDown';
 import { selectCurrentUser } from '@/store/features/auth/authSlice';
 import { useSelector } from 'react-redux';
-import { PanelLeft } from 'lucide-react';
+import { BadgeCent, PanelLeft } from 'lucide-react';
 import { useGetNotificationsQuery } from '@/store/features/notification/notificationApiService';
 import NotificationDropdown from './NotificationDropdown';
+import { useGetUserCreditStatsQuery } from '@/store/features/credit_and_payment/creditAndPaymentApiService';
 
 export default function DashboardHeader({ onToggleSidebar }) {
   const userInfo = useSelector(selectCurrentUser);
   const { data } = useGetNotificationsQuery({ read: false });
+  const { data: credits } = useGetUserCreditStatsQuery();
 
   return (
     <header className="db-header">
@@ -33,6 +35,15 @@ export default function DashboardHeader({ onToggleSidebar }) {
         </button>
       </div>
       <div className="flex items-center gap-3">
+
+        {/* Remaining Credits */}
+        <div className="flex items-center gap-1 bg-gray-50 border border-gray-200 rounded-full px-3 py-1 shadow-sm">
+          <BadgeCent className="w-5 h-5 text-primary" />
+          <span className="text-sm font-semibold text-gray-800">
+            {credits?.data?.remainingCredits ?? 0}
+          </span>
+        </div>
+
         <button className="w-8 h-8 items-center justify-center border border-gray-300 rounded-full hidden sm:flex cursor-pointer flex-shrink-0">
           <NotificationDropdown />
         </button>
