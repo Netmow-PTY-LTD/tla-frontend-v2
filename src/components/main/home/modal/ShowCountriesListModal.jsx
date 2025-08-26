d84a242ecce77b9c5b0c34d7012544d279454015
Tuhin Alom <tuhin.netmow@gmail.com>
2025-08-26 20:17:53 +0600

    country switch stopped

--- src/components/main/home/modal/ShowCountriesListModal.jsx
+++ src/components/main/home/modal/ShowCountriesListModal.jsx
@@ -29,10 +29,10 @@ export default function ShowCountriesListModal({
             return (
               <li
                 key={country.countryId || country.code}
-                className={`flex items-center gap-3 mb-2 p-2 rounded-sm cursor-pointer transition ${
+                className={`flex items-center gap-3 mb-2 p-2 rounded-sm transition ${
                   isSelected ? 'border border-[#f3f3f3]' : '' // only selected has border
                 }`}
-                onClick={() => handleSelect(country)}
+                // onClick={() => handleSelect(country)}
               >
                 {country.flag && (
                   <img
