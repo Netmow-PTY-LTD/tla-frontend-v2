fd6f10d04a838b1338d722ebcfa04941bc885254
rabby <rabby.netmow@gmail.com>
2025-08-28 10:10:00 +0600

    hide hire and pending stats from my response page in lawyer response

--- src/app/admin/list-client-details/page.jsx
+++ src/app/admin/list-client-details/page.jsx
@@ -1,11 +1,13 @@
-'use client';
+'use client'
 
 import { useGetClientWiseCasesQuery } from '@/store/features/client/ClientApiServices';
 import React from 'react';
 
-export default function ClientWiseCases() {
+export default function CaseWiseResponses() {
   const id = '68ac72eb6e7602fd7de9f7d1';
   const { data: clientWiseCases } = useGetClientWiseCasesQuery(id);
   console.log('clientWiseCases', clientWiseCases);
-  return <div>ClientWise</div>;
+
+
+  return <div>client details</div>;
 }
