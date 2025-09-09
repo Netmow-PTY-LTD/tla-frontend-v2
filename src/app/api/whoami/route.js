2f5275064d77afed41ef07818285d92cb6001cf2
Tuhin Alom <tuhin.netmow@gmail.com>
2025-09-09 17:42:15 +0600

    api route logic modified

--- src/app/api/whoami/route.js
+++ src/app/api/whoami/route.js
@@ -1,6 +1,9 @@
 import { NextResponse } from 'next/server';
 import { getClientIp, getCountry } from '@/utils/ipUtils';
 
+export const runtime = 'nodejs'; // ensure Node runtime
+export const dynamic = 'force-dynamic'; // force API re-eval
+
 export async function GET(request) {
   const ip = getClientIp(request.headers);
   const country = getCountry(request.headers);
