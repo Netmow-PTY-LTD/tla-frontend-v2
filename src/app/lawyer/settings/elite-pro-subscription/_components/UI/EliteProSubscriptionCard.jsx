986e130abd62905957c8fb731c3785568059ee81
rabby <rabby.netmow@gmail.com>
2025-10-13 10:56:27 +0600

    added optional chaning

--- src/app/lawyer/settings/elite-pro-subscription/_components/UI/EliteProSubscriptionCard.jsx
+++ src/app/lawyer/settings/elite-pro-subscription/_components/UI/EliteProSubscriptionCard.jsx
@@ -7,19 +7,19 @@ const EliteProSubscriptionCard = ({ subscription }) => {
     return (
         <div className=" bg-white shadow-md rounded-xl overflow-hidden border border-gray-200 p-6">
             <div className="flex justify-between items-center mb-4">
-                <h2 className="text-xl font-semibold text-gray-800">{subscriptionPackageId.name}</h2>
+                <h2 className="text-xl font-semibold text-gray-800">{subscriptionPackageId?.name}</h2>
                 <span
                     className={`px-3 py-1 text-sm font-medium rounded-full ${status === "active" ? "bg-green-100 text-green-800" : "bg-red-100 text-red-800"
                         }`}
                 >
-                    {status.toUpperCase()}
+                    {status?.toUpperCase()}
                 </span>
             </div>
 
             <div className="mb-4">
                 <p className="text-gray-600">
-                    <span className="font-medium">Price:</span> {subscriptionPackageId.price.amount / 100}{" "}
-                    {subscriptionPackageId.price.currency.toUpperCase()} / {subscriptionPackageId.billingCycle}
+                    <span className="font-medium">Price:</span> {subscriptionPackageId?.price.amount / 100}{" "}
+                    {subscriptionPackageId?.price.currency.toUpperCase()} / {subscriptionPackageId?.billingCycle}
                 </p>
                 <p className="text-gray-600">
                     <span className="font-medium">Period:</span>{" "}
