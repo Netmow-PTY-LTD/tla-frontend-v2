48a8686c19b049fd8879dde5e59688f2a3b88c94
Tuhin Alom <tuhin.netmow@gmail.com>
2026-02-24 11:33:32 +0600

    .eslintignore deleted

--- .eslint.config.js
+++ .eslint.config.js
@@ -1,5 +1,22 @@
-export default [
-    {
-        ignores: ["node_modules/*", "dist/", "public/*"], // adjust paths as needed
+// eslint.config.js
+export default {
+    root: true,
+    parser: "@typescript-eslint/parser",  // must be a string
+    parserOptions: {
+        ecmaVersion: 2020,
+        sourceType: "module",
+        ecmaFeatures: {
+            jsx: true,
+        },
     },
-]
+    plugins: ["@typescript-eslint"],
+    extends: [
+        "eslint:recommended",
+        "plugin:@typescript-eslint/recommended",
+        "next/core-web-vitals"
+    ],
+    rules: {
+        // your rules
+    },
+    ignores: ["node_modules/**", "dist/**", "public/**"], // replaces .eslintignore
+};
