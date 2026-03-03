ac3c7dbe63f23e3c71e3c20bd9f32f166bb54af4
rabby <rabby.netmow@gmail.com>
2026-03-03 14:59:05 +0600

    fixed issues of sub invoice invoice modal

--- src/app/lawyer/settings/subscription/_components/modal/SubInvoiceModal.jsx
+++ src/app/lawyer/settings/subscription/_components/modal/SubInvoiceModal.jsx
@@ -1,7 +1,7 @@
 import { Modal } from '@/components/UIComponents/Modal';
 import React from 'react';
 
-const InvoiceModal = ({ open, setOpen, transaction }) => {
+const SubInvoiceModal = ({ open, setOpen, transaction }) => {
   if (!transaction) return null;
 
   const {
@@ -130,4 +130,4 @@ const InvoiceModal = ({ open, setOpen, transaction }) => {
   );
 };
 
-export default InvoiceModal;
+export default SubInvoiceModal;
